26b0716d19b0f6b26d0f3ea54013cbc1ff965b56
Michelle Hoelzer <hoelzermm@gmail.com>
2019-06-01 22:23:09 -0400

    made login formview instead of just view

--- twango/authentication/views.py
+++ twango/authentication/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, HttpResponseRedirect
 from twango.authentication.forms import LoginForm
 from django.contrib.auth import authenticate, login, logout
 from django.views import View
+from django.views.generic.edit import FormView
 
 
-class LoginView(View):
+class LoginView(FormView):
     template_name = "../templates/generic.html"
     form_class = LoginForm
     url_redirect = "/"
@@ -18,17 +19,15 @@ class LoginView(View):
                        "button_value": self.button_value}
                       )
 
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
-            user = authenticate(
-                username=data["username"], password=data["password"])
-            if user is not None:
-                login(request, user)
-                return HttpResponseRedirect(
-                    request.GET.get("next", self.url_redirect))
-        return render(request, self.template_name,
+    def form_valid(self, form):
+        data = form.cleaned_data
+        user = authenticate(
+            username=data["username"], password=data["password"])
+        if user is not None:
+            login(self.request, user)
+            return HttpResponseRedirect(
+                self.request.GET.get("next", self.url_redirect))
+        return render(self.request, self.template_name,
                       {"header": self.header, "form": form,
                        "button_value": self.button_value}
                       )
