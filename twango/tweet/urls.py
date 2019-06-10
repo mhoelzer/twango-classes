923bf11df603af9c5fc28edf878fd0ce64272380
Michelle Hoelzer <hoelzermm@gmail.com>
2019-06-10 15:46:34 -0400

    added as_view() to urls; should function

--- twango/tweet/urls.py
+++ twango/tweet/urls.py
@@ -6,6 +6,6 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path("twang/", login_required(TwangCreationView.as_view()),
          name="twangcreate"),
-    path("twangs/", TwangView),
-    path("twang/<int:id>/", TwangView, name="individual")
+    path("twangs/", TwangView.as_view()),
+    path("twang/<int:id>/", TwangView.as_view(), name="individual")
 ]
